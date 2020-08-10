9d2fca2e32c622dedfe819460adde2dc3801560c
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-08-10 23:48:01 +0800

    Fixed errors

--- data_process.py
+++ data_process.py
@@ -1,21 +1,23 @@
+from imblearn.over_sampling import SMOTE
+from pathlib import Path
+# from collections import Counter
+
 import psycopg2
 import numpy as np
-from imblearn.over_sampling import SMOTE
-# from collections import Counter
+import getpass
 
 np.set_printoptions(threshold=np.inf)
 
 
 class data_process():
-    def __init__(self, data_base, res):
+    def __init__(self, data_base):
         self.data_base = data_base
-        self.res = res
-		# the 11 measurements of blood gas in MIMIC-III database
+        # the 11 measurements of blood gas in MIMIC-III database
         if self.data_base == 'mimic':
             self.dict_lab = {50882: 0, 50902: 1, 50912: 2, 50931: 3, 50960: 4, 50971: 5, 50983: 6, 51006: 7, 51222: 8,
                              51265: 9, 51301: 10}
             self.dim = 16
-		# the 10 measurements of blood gas in eICU database
+        # the 10 measurements of blood gas in eICU database
         elif self.data_base == 'eicu':
             self.dict_lab = {'bicarbonate': 0, 'chloride': 1, 'creatinine': 2, 'glucose': 3, 'magnesium': 4,
                              'potassium': 5, 'sodium': 6, 'Hgb': 7, 'platelets x 1000': 8, 'WBC x 1000': 9}
@@ -23,14 +25,16 @@ class data_process():
 
     def data_num(self, conn):
         cur = conn.cursor()
-		# write the Select sentence with your own database name 
+        # write the Select sentence with your own database name
         if self.data_base == 'mimic':
-            cur.execute("with s1 as(select distinct icustay_id from mimiciii.cohort_modify) select count(*) from s1")
+            cur.execute(
+                "with s1 as(select distinct icustay_id from mimiciii.cohort_modify) select count(*) from s1")
         elif self.data_base == 'eicu':
-            cur.execute("with s1 as(select distinct patientunitstayid from eicu.cohort_modify) select count(*) from s1")
+            cur.execute(
+                "with s1 as(select distinct patientunitstayid from eicu.cohort_modify) select count(*) from s1")
         rows = cur.fetchall()
         # get the number of patient in the database
-		pat_num = rows[0]
+        pat_num = rows[0]
         conn.commit()
         cur.close()
         # print(pat_num[0])
@@ -42,7 +46,7 @@ class data_process():
         label = np.zeros((self.pat_num,), dtype=np.float)
         i = 0
         cur = conn.cursor()
-		# write the Select sentence with your own database name
+        # write the Select sentence with your own database name
         if self.data_base == 'mimic':
             cur.execute(
                 "select distinct icustay_id, gender, age, mean_weight, height, bmi_group, aki_stage from mimiciii.cohort_modify order by icustay_id")
@@ -51,7 +55,7 @@ class data_process():
                 "select distinct patientunitstayid, gender, age, weight, height, bmi_group, aki_stage from eicu.cohort_modify order by patientunitstayid")
 
         rows = cur.fetchall()
-		# write the results of the Select to an array named 'data' 
+        # write the results of the Select to an array named 'data'
         for row in rows:
             for j in range(5):
                 data[i][j] = row[j + 1]
@@ -61,7 +65,7 @@ class data_process():
         # print(label)
         i = 0
         j = 0
-		
+
         if self.data_base == 'mimic':
             cur.execute(
                 "select distinct icustay_id, itemid, valuenum from mimiciii.cohort_modify order by icustay_id, itemid")
@@ -70,7 +74,7 @@ class data_process():
                 "select distinct patientunitstayid, labname, labresult from eicu.cohort_modify order by patientunitstayid, labname")
 
         rows = cur.fetchall()
-		# write the measurements of blood gas to an array named 'data'
+        # write the measurements of blood gas to an array named 'data'
         for row in rows:
             if row[1] in self.dict_lab:
                 # print(row[1])
@@ -88,35 +92,50 @@ class data_process():
         cur.close()
 
         return data, label
-	# OverSample using SMOTE algorithm
+    # OverSample using SMOTE algorithm
+
     def OverSamp(self, data_tr, label_tr):
-        oversampler = SMOTE(ratio='auto', random_state=np.random.randint(100), k_neighbors=5, m_neighbors=10,
-                            kind='regular', n_jobs=1)
+        oversampler = SMOTE(
+            sampling_strategy='auto',
+            random_state=np.random.randint(100),
+            k_neighbors=5,
+            n_jobs=1,
+        )
 
         for i in range(len(data_tr)):
             for j in range(self.dim):
                 if np.isnan(data_tr[i][j]):
                     data_tr[i][j] = 0
+
         data_tr_res, label_tr_res = oversampler.fit_sample(data_tr, label_tr)
         # print('Resampled dataset shape {}'.format(Counter(label_tr_res)))
         return data_tr_res, label_tr_res
-		
-	# running this method, every patient can be represented as a picture with the dimension of 1×self.dim
+
+    # running this method, every patient can be represented as a picture with the dimension of 1×self.dim
     def run(self):
-        if self.data_base == 'mimic':
-            conn = psycopg2.connect(database='mimic', user='postgres', password='postgres', host='localhost',
-                                    port='5432')
-        elif self.data_base == 'eicu':
-            conn = psycopg2.connect(database='postgres', user='postgres', password='postgres', host='localhost',
-                                    port='5432')
+        conn = psycopg2.connect(
+            database='aki',
+            user=getpass.getuser(),
+            password='',
+            host='localhost',
+            port='5432',
+        )
         print("Opened database successfully")
 
         self.pat_num = self.data_num(conn)
         data, label = self.read_data(conn)
-        if self.res == 'SMOTE':
-            data_res, label_res = self.OverSamp(data, label)
+        data_res, label_res = self.OverSamp(data, label)
         pat_num_res = len(label_res)
-        np.savez("./data_set/"+(str(self.data_base) + "_processed_1D.npz"), pat_num_res, data_res, label_res)
+
+        # ensure output dir exists
+        output_dir = Path('data_set')
+        output_dir.mkdir(parents=False, exist_ok=True)
+
+        # save output file
+        output_file = f'{self.data_base}_processed_1D.npz'
+        output_path = output_dir / output_file
+        np.savez(output_path, pat_num_res, data_res, label_res)
+
         # data = np.load(str(self.data_base) + "_processed_1D.npz")
         # print('pat_num_res is', data["arr_0"])
         # print('data_res num is', data["arr_1"].shape)
